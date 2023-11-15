00e45e5e66638f4503ad0ce9b02a810f3c197a78
Zachary Zaro <zach@withcoherence.com>
2023-11-15 01:12:55 +0000

    config

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.COHERENCE_CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.COHERENCE_CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
